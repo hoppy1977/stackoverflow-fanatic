d6658d5a0a314ed2ddd2ef382fa809385e35a302
Shane Hopcroft <shanehopcroft@hotmail.com>
2019-09-28 15:18:15 +1000

    General development

--- handler.js
+++ handler.js
@@ -11,8 +11,7 @@ module.exports.login = async (event, context) => {
   console.log("Logging into stackoverflow.com");
 
   let driver = await new Builder().forBrowser('chrome').build();
-//  driver.manage().timeouts().implicitlyWait();
-driver.manage().getTimeouts().implicitlyWait = true;
+  driver.manage().getTimeouts().implicitlyWait = true;
 
   try {
     await driver.get("https://stackoverflow.com");
@@ -22,23 +21,43 @@ driver.manage().getTimeouts().implicitlyWait = true;
     await driver.findElement(By.id("password")).sendKeys(password);
     await driver.findElement(By.id("submit-button")).click();
 
-    await driver.findElement(By.className("my-profile js-gps-track")).click();
-    // let avatarElement = await driver.findElement(By.ClassName("mini-avatar"));
+    await driver.wait(until.elementLocated(By.className("my-profile js-gps-track")), 5 * 1000).then(el => {
+      driver.findElement(By.className("my-profile js-gps-track")).click();
+    });
 
-    // if (avatarElement.text == displayName) {
-    //   console.log("Logged into stackoverflow.com and accessed profile page");
-    // } else {
-    //   console.log("Error: Something went wrong!");
-    // }
+    await driver.wait(until.elementLocated(By.className("mini-avatar")), 5 * 1000).then(el => {
+      console.log("x");
+      let avatarElement = driver.findElement(By.className("mini-avatar"));
+      console.log("y");
+      console.log(avatarElement);
 
-//    await driver.sleep(2000);
+      console.log("z1");
+      avatarElement.getText().then(function(actualDisplayName) {
+        console.log("z2");
 
+        console.log(actualDisplayName);
+
+        if (actualDisplayName == displayName) {
+          console.log("Logged into stackoverflow.com and accessed profile page");
+        } else {
+          console.log(`Error: We were expecting the profile name to be '${displayName}', but it was '${actualDisplayName}'!`);
+        }
+      })
+      .catch(x => {
+        console.log(x);  
+      });
+      
+    }).catch(x => {
+      console.log("Error: Something went wrong!");
+      console.log(x);
+    });
+    
   } finally {
     console.log("Terminating driver");
     await driver.quit();
   }
 
-
+  driver.sleep(10000);
 
   return {
     statusCode: 200,
