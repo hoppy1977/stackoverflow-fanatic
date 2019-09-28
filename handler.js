f06f08b06b1e7cf1142d93a44730aa2d83a7c436
Shane Hopcroft <shanehopcroft@hotmail.com>
2019-09-28 18:06:48 +1000

    General development

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 const email = "shanehopcroft@hotmail.com";
-const password = "<REDACTED>";
+const password = "shane1977";
 const displayName = "Shane Hopcroft";
 
 module.exports.login = async (event, context) => {
@@ -12,6 +12,7 @@ module.exports.login = async (event, context) => {
 
   let driver = await new Builder().forBrowser('chrome').build();
   driver.manage().getTimeouts().implicitlyWait = true;
+  driver.setPageLoadStrategy = 'normal';
 
   try {
     await driver.get("https://stackoverflow.com");
@@ -21,37 +22,19 @@ module.exports.login = async (event, context) => {
     await driver.findElement(By.id("password")).sendKeys(password);
     await driver.findElement(By.id("submit-button")).click();
 
-    await driver.wait(until.elementLocated(By.className("my-profile js-gps-track")), 5 * 1000).then(el => {
-      driver.findElement(By.className("my-profile js-gps-track")).click();
-    });
-
-    await driver.wait(until.elementLocated(By.className("mini-avatar")), 5 * 1000).then(el => {
-      console.log("x");
-      let avatarElement = driver.findElement(By.className("mini-avatar"));
-      console.log("y");
-      console.log(avatarElement);
-
-      console.log("z1");
-      avatarElement.getText().then(function(actualDisplayName) {
-        console.log("z2");
-
-        console.log(actualDisplayName);
-
-        if (actualDisplayName == displayName) {
-          console.log("Logged into stackoverflow.com and accessed profile page");
-        } else {
-          console.log(`Error: We were expecting the profile name to be '${displayName}', but it was '${actualDisplayName}'!`);
-        }
-      })
-      .catch(x => {
-        console.log(x);  
+    await driver
+      .wait(until.elementLocated(By.className("my-profile js-gps-track")), 5 * 1000)
+      .then(console.log("aaa"))
+      .then(driver.findElement(By.className("my-profile js-gps-track")).click())
+      .then(console.log("bbb"))
+      //.then(driver.wait(until.elementLocated(By.className("mini-avatar")), 5 * 1000))
+      //.then(avatarElement => avatarElement.findElement(By.className("name")))
+      //.then(avatarNameElement => avatarNameElement.getText())
+      .then(console.log("Whaaaat?!!?"))
+      .catch(ex => {
+          console.log(ex);
       });
-      
-    }).catch(x => {
-      console.log("Error: Something went wrong!");
-      console.log(x);
-    });
-    
+      //;      
   } finally {
     console.log("Terminating driver");
     await driver.quit();
