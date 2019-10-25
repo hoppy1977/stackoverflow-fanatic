0cd0a97db9bb8d2c6fc5ac2896cc6beb648e79cc
Shane Hopcroft <shanehopcroft@hotmail.com>
2019-10-25 22:39:05 +1100

    Now implementing chrome-aws-lambda example code

--- handler.js
+++ handler.js
@@ -1,70 +1,35 @@
 'use strict';
 
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 module.exports.login = async (event, context) => {
 
   console.log("Logging into stackoverflow.com");
 
-  //puppeteer.launch()
-  await puppeteer.launch({headless: false})
-    .then(async browser => {
+  let result = null;
+  let browser = null;
 
-      const page = await browser.newPage();
-      await page.setViewport({ width: 1280, height: 800 })
-      
-      await page.goto('https://www.stackoverflow.com', {waitUntil: 'networkidle2'});
-    
-      // Click on the 'Log in' button
-      await Promise.all([
-        page.click('body > header > div > ol.-secondary.js-secondary-topbar-links.drop-icons-responsively.user-logged-out.the-js-is-handling-responsiveness > li.-ctas > a.login-link.s-btn.btn-topbar-clear.py8.js-gps-track'),
-        page.waitForNavigation(),    
-      ]);
-    
-      // Type the email
-      await page.focus('#email')
-      await page.keyboard.type(process.env.STACKOVERFLOW_EMAIL);
-      
-      // Type the password
-      await page.focus('#password')
-      await page.keyboard.type(process.env.STACKOVERFLOW_PASSWORD);
-    
-      // Click submit
-      await Promise.all([
-        page.click('#submit-button'),
-        page.waitForNavigation(),
-      ]);  
-    
-      // Click on the profile icon
-      await Promise.all([
-        page.click('body > header > div > ol.-secondary.js-secondary-topbar-links.drop-icons-responsively.user-logged-in.the-js-is-handling-responsiveness > li:nth-child(2) > a'),
-        page.waitForNavigation(),
-      ]) 
-
-      //await page.screenshot({ path: 'form.png', fullPage: true });
-
-      // Work out what the display name is
-      let actualDisplayName = await page.$eval('#mainbar-full > div.subheader.reloaded.js-user-header > div.mini-avatar > div.name', e => e.innerText);
-
-      await browser.close();
-      
-      return actualDisplayName;
-    }).then((actualDisplayName) => {
-      const expectedDisplayName = process.env.STACKOVERFLOW_DISPLAYNAME;
-      if(actualDisplayName == expectedDisplayName) {
-        console.log("Logged into stackoverflow.com and accessed profile page")
-      } else {
-        console.error(`Error: We were expecting the profile name to be '${expectedDisplayName}', but it was '${actualDisplayName}'`);
-      }
-    }).catch((ex) => {
-      console.log(ex)
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
     });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Ferrets are cool',
-      input: event,
-    }),
-  };
+    let page = await browser.newPage();
+
+    await page.goto(event.url || 'https://shanehopcroft.com');
+    result = await page.title();
+
+  } catch (error) {
+    return context.fail(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  console.log("All good");
+  return context.succeed(result);
 };
